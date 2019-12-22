e35eb787592683e95748caae0fb7fc396319ef0b
noracyrd <norabyrd@alumni.upenn.edu>
2019-12-22 12:52:29 -0600

    shell files

---
+++ client/build/precache-manifest.38d72e3af4b71776ddd9e9904d0037ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eec2000968f9eb82a64a8e16fed2285",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3040b8dd9a35d70d605",
+    "url": "/static/css/main.d618d283.chunk.css"
+  },
+  {
+    "revision": "4f896f1b780fd336ab45",
+    "url": "/static/js/2.11c5a6b8.chunk.js"
+  },
+  {
+    "revision": "f3040b8dd9a35d70d605",
+    "url": "/static/js/main.8afc6170.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8022;
+const path = require("path");
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+ app.use(express.static("client/build"));
+}
+
+// rewrite
+app.use("/", function(req,res) {
+    res.sendFile(path.join(__dirname,"./public/index.html"))
+})
+
+app.listen(PORT, function() {
+    console.log(`🌎  ==> Server now listening on PORT ${PORT}!`);
+  });
