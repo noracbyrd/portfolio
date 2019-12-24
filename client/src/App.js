0fad9a2af33f4f63f20ab1f53d23a72f4ed6ab17
noracyrd <norabyrd@alumni.upenn.edu>
2019-12-23 22:31:31 -0600

    starter shell redo

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-
 function App() {
-  return(
+  return (
     <div className="App">
+
     </div>
-  )
+  );
 }
-
-
 
 export default App;
