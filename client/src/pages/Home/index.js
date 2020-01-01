c6100d23fdd2204fe0ff111cd41022f96fbc1614
noracyrd <norabyrd@alumni.upenn.edu>
2019-12-31 18:11:36 -0600

    started working on home page

---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Navbar from "./components/Navbar"
+import About from "./components/About"
+import Portfolio from "./components/Portfolio"
+import Contact from "./components/Contact"
+import "./App.css"
+// import { Route } from "react-router-dom"
+
+
+class Home extends Component {
+    state = {
+        navTitle: "Nora Byrd ~ Full Stack Developer"
+    }
+    render(){
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="container">
+        <About />
+        <br></br>
+        <br></br>
+        <Portfolio />
+        <br></br>
+        <br></br>
+        <Contact />
+        <div className="row">
+          <div className="secretLink">
+            <a><img alt="ncb logo" src="http://ryanabney.com/nora/ncb.png"></img></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+}
+
+export default Home;
