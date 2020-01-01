40951ddd5b8dfc2de547d9bafee8f2ebdedefa0c
noracyrd <norabyrd@alumni.upenn.edu>
2019-12-31 18:00:05 -0600

    tweaked app file

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import About from "./components/About"
 import Portfolio from "./components/Portfolio"
 import Contact from "./components/Contact"
 import "./App.css"
-import { Route } from "react-router-dom"
+// import { Route } from "react-router-dom"
 
 
 function App() {
